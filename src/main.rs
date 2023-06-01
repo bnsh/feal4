571abefa0cd4c984d40e5154befd12c0caa7c2cc
Binesh Bannerjee <binesh_binesh@hotmail.com>
2023-05-31 22:23:45 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+// vim: expandtab shiftwidth=4 tabstop=4:
+
+/* This will be my implementation of FEAL-4 in Rust. Primarily my goal here
+ * is to try out what's described in this YouTube video: https://www.youtube.com/watch?v=xav-GUO_o4s
+ * FEAL on Wikipedia: https://en.wikipedia.org/wiki/FEAL
+ * Actually, "Applied Cryptography" by Bruce Schneier has a section on FEAL in Chapter 13.4 (pp 308 on my copy).
+ */
+
+fn gx(x: u8, a: u8, b: u8) -> u8 {
+    // gx(a, b) = rotate left two bits((a+b+x) mod 256)
+    let int = a.wrapping_add(b).wrapping_add(x);
+    let rot = ((int << 2) & 0xfc) | ((int & 0xc0) >> 6);
+    rot
+}
+
+fn g0(inp1: u8, inp2: u8) -> u8 { gx(0, inp1, inp2) }
+fn g1(inp1: u8, inp2: u8) -> u8 { gx(1, inp1, inp2) }
+
+fn f(a: u8, b: u8, c: u8, d: u8) {
+    // "Applied Cryptography" Bruce Schneier 13.4 Figure 13.4
+    let v1 = a ^ b;
+    let v2 = c ^ d;
+    let v3 = g1(v1, v2);
+    let v4 = g0(v3, v4);
+    let v5 = g0(a, v3);
+    let v6 = g1(d, v4);
+    let ap = v5;
+    let bp = v3;
+    let cp = v4;
+    let dp = v6;
+    (ap, bp, cp, dp)
+}
+
+fn fk(
+
+fn main() {
+}
